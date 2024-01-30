cab196d4d5e973d9d666613864755ac785c317cd
root <root@DESKTOP-3OC5LSV>
2024-01-30 04:18:38 -0700

    Final

--- model_card_template.md
+++ model_card_template.md
@@ -1,18 +1,23 @@
 # Model Card
 
-For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
-
 ## Model Details
+A binary classification model for census income predictions by Joshua Batis
 
 ## Intended Use
+To generate predictons for general decision making regarding households whose income exceeds $50,000.  Grouped by various demographic and employment details.
 
 ## Training Data
+The data comes from census data gathered by UC Irvine Machine Learning Repository. It includes various features such as workclass, education, marital-status, occupation, relationship, race, sex, and native-country. 19% of the data was used for random testing and the random state was set to 72 for reproducibility.
 
 ## Evaluation Data
+19% of the data was used for our model testing and it was preprocessed at the same time as our training data.
 
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+The precision, recall, and F1 scores were used on our model. These measures were chosen to measure our pipeline's performance.
+Precision: 0.7461 | Recall: 0.6257 | F1: 0.6806
 
 ## Ethical Considerations
+Bias may be found in the data depending on the demographic of the data and other factors that were not considered in the questioning.  This data does not reflect the population as a whole.
 
 ## Caveats and Recommendations
+This data is not up to date and inflation has increased significantly.  These should be taken into account and the binary variable should be adjusted with current inflation.  The more up to date the data is the better.
--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,73 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+import numpy as np
+from ml.data import process_data
+from ml.model import compute_model_metrics, train_model, inference
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
-    # add description for the first test
+    Test the machine learning model's algorithm.
     """
-    # Your code here
-    pass
+    # Create test dataset
+    data = pd.DataFrame(
+        {
+            'column_1' : [5, 4, 3, 2, 1],
+            'column_2' : [0, 1, 0, 1, 0],
+            'label' : [0, 1, 0, 1, 0]
+        }
+    )
+
+    # Process data
+    X_train, y_train, _,_ = process_data(
+        data,
+        categorical_features= ['column_2'],
+        label= 'label',
+        training= True
+    )
+
+    #Train model
+    random_forest = train_model(X_train, y_train)
+
+    # Check returned model type
+    returned_model = RandomForestClassifier
+    assert isinstance(random_forest, returned_model)
 
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
     """
-    # add description for the second test
+    Test if the compute_model_metrics function returns expected values.
     """
-    # Your code here
-    pass
+    # Create test set
+    y_true = np.array([0, 1, 0, 1, 0])
+    y_pred = np.array([0, 1, 1, 1, 0])
 
+    # Compute metrics
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+
+    # Check if the computed metrics match the expected values
+    expected_precision = 0.6667
+    expected_recall = 1.0
+    expected_fbeta = 0.8
+
+    assert round(precision, 4) == expected_precision
+    assert round(recall, 4) == expected_recall
+    assert round(fbeta, 4) == expected_fbeta
 
 # TODO: implement the third test. Change the function name and input as needed
 def test_three():
     """
-    # add description for the third test
-    """
-    # Your code here
-    pass
+      Test inference of model
+      """
+    X_train = np.random.rand(60, 5)
+    y_train = np.random.randint(2, size=60)
+    random_forest = train_model(X_train, y_train)
+    y_preds = inference(random_forest, X_train)
+
+    assert y_train.shape == y_preds.shape
